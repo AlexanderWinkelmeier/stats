cc84175bba4379259c938f04ceff8b4a10d99d33
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2024-08-30 09:34:41 +0200

    Reusable Code in TypeScript

---
+++ build/analyzers/WinsAnalyzis.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WinsAnalyzis = void 0;
+class WinsAnalyzis {
+}
+exports.WinsAnalyzis = WinsAnalyzis;
---
+++ build/reportTargets/HtmlReports.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HtmlReport = void 0;
+class HtmlReport {
+    print(report) {
+        console.log(report);
+    }
+}
+exports.HtmlReport = HtmlReport;
